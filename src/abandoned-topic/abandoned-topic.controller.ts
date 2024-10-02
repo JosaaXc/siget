3dc5efe5518f51df759818e456e0f0024d122a33
JosaaXC <josafat719@gmail.com>
2024-10-02 14:38:47 -0600

    refactor: Add get-by-degree-program endpoint to AbandonedTopicController

--- src/abandoned-topic/abandoned-topic.controller.ts
+++ src/abandoned-topic/abandoned-topic.controller.ts
@@ -1,7 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Query } from '@nestjs/common';
 import { AbandonedTopicService } from './abandoned-topic.service';
+import { Auth } from '../auth/decorators';
+import { ValidRoles } from '../auth/interfaces';
+import { DegreeProgramDto } from '../accepted-topics/dto/degree-program.dto';
+import { PaginationDto } from '../common/dtos/pagination.dto';
 
 @Controller('abandoned-topic')
 export class AbandonedTopicController {
   constructor(private readonly abandonedTopicService: AbandonedTopicService) {}
+
+
+  @Post('get-by-degree-program')
+  @Auth(ValidRoles.student)
+  create(
+    @Query() paginationDto: PaginationDto,
+    @Body() degreeProgramDto: DegreeProgramDto
+  ) {
+    return this.abandonedTopicService.findByDegreeProgram( degreeProgramDto, paginationDto );
+  }
+  
 }
--- src/abandoned-topic/abandoned-topic.service.ts
+++ src/abandoned-topic/abandoned-topic.service.ts
@@ -1,4 +1,49 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { AbandonedTopic } from './entities/abandoned-topic.entity';
+import { DegreeProgramDto } from '../accepted-topics/dto/degree-program.dto';
+import { PaginationDto } from '../common/dtos/pagination.dto';
+import { UserInformation } from '../user-information/entities/user-information.entity';
 
 @Injectable()
-export class AbandonedTopicService {}
+export class AbandonedTopicService {
+
+    constructor(
+        @InjectRepository(AbandonedTopic)
+        private readonly abandonedTopicRepository: Repository<AbandonedTopic>,
+        @InjectRepository(UserInformation)
+        private readonly userInformationRepository: Repository<UserInformation>,
+    ) {}
+
+    async findByDegreeProgram({ degree }:  DegreeProgramDto, { limit = 10, offset = 0 }: PaginationDto) {
+        const abandonedTopics = await this.abandonedTopicRepository.createQueryBuilder('abandonedTopic')
+        .leftJoinAndSelect('abandonedTopic.acceptedBy', 'acceptedBy')
+        .leftJoinAndSelect('abandonedTopic.degreeProgram', 'degreeProgram')
+        .leftJoinAndSelect('abandonedTopic.graduationOption', 'graduationOption')
+        .where('abandonedTopic.degreeProgramId IN (:...degree)', { degree })
+        .skip(offset)
+        .take(limit)
+        .getMany();
+
+        const topicsWithUserInfo = await Promise.all(abandonedTopics.map(async (topic) => {
+            const userInfo = await this.getUserInformation(topic.acceptedBy.id);
+            return {
+            ...topic,
+            acceptedBy: {
+                ...topic.acceptedBy,
+                ...userInfo,
+            },
+            };
+        }));
+        return topicsWithUserInfo;
+    }
+
+    private async getUserInformation(userId: string) {
+        return await this.userInformationRepository.createQueryBuilder("userInformation")
+          .innerJoin("userInformation.user", "user", "user.id = :id", { id: userId })
+          .select(['userInformation.name', 'userInformation.fatherLastName', 'userInformation.motherLastName'])
+          .getOne();
+    }
+
+}
